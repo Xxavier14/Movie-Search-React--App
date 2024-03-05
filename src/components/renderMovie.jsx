e42652c4eb300add3f9640e82ab9ca177e1dbe47
Juan P Javier <juanpablojavier14@gmail.com>
2024-03-04 21:07:22 -0300

    RenderMovie

--- src/components/renderMovie.jsx
+++ src/components/renderMovie.jsx
@@ -4,7 +4,6 @@ export const renderMovie = (movies) => {
             {movies.map((movie) => (
                 <li key={movie.imdbID}>
                     <h3>{movie.Title}</h3>
-                    <p>{movie.Type}</p>
                     <p>{movie.Year}</p>
                     <img src={movie.Poster} alt={movie.Title} />
                 </li>
